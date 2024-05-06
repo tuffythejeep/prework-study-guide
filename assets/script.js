8a660120946615210ac52820e9877e5c14b82909
tuffythejeep <167649421+tuffythejeep@users.noreply.github.com>
2024-05-05 21:56:26 -0500

    const shapes

--- assets/script.js
+++ assets/script.js
@@ -11,3 +11,8 @@ if (topic === 'HTML') {
 } else {
   console.log('Please try again!');
 }
+
+const shapes = ["triangle", "square", "pentagon", "circle"];
+for(let x = 0; x < shapes.length; x++) {
+ console.log(shapes[x]);
+}
